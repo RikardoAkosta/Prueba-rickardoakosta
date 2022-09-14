1f1009299035a65885a5b0fea880a31ebff2b4b3
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-09-14 21:03:53 +0200

    se crean componentes y rutas

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ Para tener un proyecto escalable con el tiempo lo primero es crear dos carpetas:
 -componentes: aqui estaran los componentes generales los cuales se dividen en otras carpetas compuestas con archivos index.js seran modulos de exportacion para reducir las lineas al momento de exportar los componentes
 
 -pages: en esta estaran componentes que solo se usaran en la pagina local
+
+## Definicion de las rutas
+
+se hace una definicion de las rutas que se desean donde el dominio del API sera el mismo para todas:
+--Obtenr el listado de los productos
+--Obtener el detalle del producto
+--añadir pruduxtos al carrito de compra
--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Home, Product, Purchases, Login, SignUp } from "./pages";
 
 function App() {
-  return <></>;
+  return (
+    <HashRouter>
+      {/* Rutas privadas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Purchases />} />
+
+        {/* Rutas Publicas */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
