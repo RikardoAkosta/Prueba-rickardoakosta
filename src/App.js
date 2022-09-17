a7b9e9eb70b2a65175744bbde671703b27f0620a
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-09-17 11:17:53 +0200

    se crean la carpeta para crear las rutas con los endpoints y usar redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { Home, Product, Purchases, Login, SignUp } from "./pages";
+import { Home, Product, Purchases, Login } from "./pages";
 import { Container } from "react-bootstrap";
 import { LoadingScreen } from "./components";
+import { useSelector } from "react-redux";
 
 function App() {
+  const isLoading = useSelector(state => state.isLoading);
   return (
     <HashRouter>
       <Container>
-        <LoadingScreen />
+        {isLoading && <LoadingScreen />}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product/:id" element={<Product />} />
           <Route path="/cart" element={<Purchases />} />
 
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Container>
     </HashRouter>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,4 +2,3 @@ export { default as Home } from "./Home";
 export { default as Login } from "./Login";
 export { default as Product } from "./Product";
 export { default as Purchases } from "./Purchases";
-export { default as SignUp } from "./SignUp";
