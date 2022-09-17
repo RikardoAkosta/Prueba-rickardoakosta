e9f76b9f6dfe481e7409216c898f017290813273
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-09-17 17:51:14 +0200

    se generan las card y se traen la informacion con el navigate para el diseño de la card usamos bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,29 +3,38 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getProduct } from "../store/slice/product.slice";
 import { useDispatch, useSelector } from "react-redux";
-import { Card } from "react-bootstrap";
+import { Card, Col, Row } from "react-bootstrap";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
 
   const product = useSelector(state => state.product);
 
   useEffect(() => {
     dispatch(getProduct());
-
   }, []);
 
   return (
     <div>
-      <h1>Home</h1> 
-      {product.map(productItem => (
-        <Card onClik = {() => navigate (`/product/${productItem.id}`)} >{/*hacemos que sea código dinamico*/}
-          {productItem.headline}
-          <img src={productItem.image} alt="" />
-        </Card>
-      ))
-      }   
+      <h1>Home</h1>
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {product.map(productItem => (
+          <Col>
+            <Card
+              style={{ cursor: "pointer" }}
+              onClik={() => navigate(`/product/${productItem.id}`)}
+            >
+              {/*hacemos que sea código dinamico*/}
+              <Card.Img variant="top" src={productItem.image} />
+              <Card.Body>
+                <Card.Title>{productItem.headline}</Card.Title>
+                <Card.Text>{productItem.lead}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 };
