0826676eb44b13e3e041c5e97c9b4bb9dfa998a3
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-09-18 22:52:13 +0200

    hacemos el diseño para la pantalla de login y captamos lo datos generados

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { Button, Card, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const submit = data => {
+    console.log(data);
+  };
   return (
     <div>
-      <h1>Login</h1>
+      <Card style={{ maxWidth: "500px" }} className="mx-auto">
+        <Card.Body>
+          <h1>Login</h1>
+          <Form onSubmit={handleSubmit(submit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                {...register("email")}
+                type="email"
+                placeholder="Enter email"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                {...register("password")}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
