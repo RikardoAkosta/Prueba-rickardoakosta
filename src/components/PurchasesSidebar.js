bb9eb8015e40258dd95b7a46aafa4374a16d2a10
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-09-22 21:48:39 +0200

    hecemos el componente para generar el el carro de compras sidebar

--- src/components/PurchasesSidebar.js
+++ src/components/PurchasesSidebar.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { Offcanvas, ListGroup } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const PurchasesSidebar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const PurchasesSidebar = ({ show, handleClose }) => {
+  const purchases = useSelector(state => state.purchases);
+
+  const navigate = useNavigate();
+
+  const selectProduct = purchases => {
+    handleClose();
+    navigate(`/news/${purchases.product.id}`);
+  };
+
+  return (
+    <div>
+      <Offcanvas show={show} onHide={handleClose} placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Favorites</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ListGroup variant="flush">
+            {purchases.map(purchase => (
+              <ListGroup.Item onClick={() => selectProduct(purchases)}>
+                {purchase.product.id}
+                <img
+                  src={purchase.product.imgUrl}
+                  className="img-fluid"
+                  alt=""
+                />
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
 };
 
 export default PurchasesSidebar;
+ 
